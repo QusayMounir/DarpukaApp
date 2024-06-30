7a2c73c58dcb1457b3581fc74f6a723b0e7b3567
Qusay Munir <Qformatik@hotmail.com>
2024-06-30 17:24:05 +0100

    Add project files.

---
+++ DarpukaApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DarpukaApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new DarbukaAppPage();
+        }
+    }
+}
---
+++ DarpukaApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DarpukaApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
